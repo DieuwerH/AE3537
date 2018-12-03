ba87e00b3755709f10f5bfb5df16d14575f7c703
Dieuwer Hondelink <dieuwer.hondelink@gmail.com>
2018-12-03 23:22:11 +0100

    More helper functions, now also able to specify color and marker

--- PlotHelper.py
+++ PlotHelper.py
@@ -1,13 +1,30 @@
 
 
-def scatterStatevectors(axes, statevectors):
+def scatterStatevectors(axes, statevectors, color='none', marker='none'):
     xs = list(map(lambda x: x.posX(), statevectors))
     ys = list(map(lambda x: x.posY(), statevectors))
     zs = list(map(lambda x: x.posZ(), statevectors))
-    axes.scatter(xs, ys, zs)
+    if (color != 'none' and marker != 'none'):
+        axes.scatter(xs, ys, zs, c=color, marker=marker)
+    else:
+        axes.scatter(xs, ys, zs)
 
-def plotLine(axes, statevec_a, statevec_b, color = 'b'):
-    axes.plot(xs=[statevec_a.posX(), statevec_b.posX()],
-              ys=[statevec_a.posY(), statevec_b.posY()],
-              zs=[statevec_a.posZ(), statevec_b.posZ()],
-              c=color)
+def plotLine(axes, statevec_a, statevec_b, color = 'none'):
+    if (color != 'none'):
+        axes.plot(xs=[statevec_a.posX(), statevec_b.posX()],
+                  ys=[statevec_a.posY(), statevec_b.posY()],
+                  zs=[statevec_a.posZ(), statevec_b.posZ()],
+                  c=color)
+    else:
+        axes.plot(xs=[statevec_a.posX(), statevec_b.posX()],
+                  ys=[statevec_a.posY(), statevec_b.posY()],
+                  zs=[statevec_a.posZ(), statevec_b.posZ()])
+
+def plotLines(axes, statevecs_a, statevecs_b, color='none'):
+    for sv_a, sv_b in zip(statevecs_a, statevecs_b):
+        plotLine(axes, sv_a, sv_b, color=color)
+
+def plotAxesMarkers(ax):
+    ax.plot(xs=[0, 6000], ys=[0, 0], zs=[0, 0], c='r')
+    ax.plot(xs=[0, 0], ys=[0, 6000], zs=[0, 0], c='g')
+    ax.plot(xs=[0, 0], ys=[0, 0], zs=[0, 6000], c='b')
