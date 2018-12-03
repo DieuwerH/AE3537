7a1209a9326f1a8a02836983f5f5890dd3982fbd
Dieuwer Hondelink <dieuwer.hondelink@gmail.com>
2018-12-03 23:23:14 +0100

    Changed some functions, more use of plothelper, file should be cleaner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime
 import tle_functions
+import PlotHelper
 import yaml
 import yaml_reader
 from sgp4.earth_gravity import wgs72
@@ -26,40 +27,46 @@ ut1 = 86164.090530
 
 doptrack = LatLon(51.99888889, 4.37333333, height=134.64, name='DopTrackStation').toCartesian()
 # From http://www.apsalin.com/convert-geodetic-to-cartesian.aspx :
-dopStateVec = StateVector(3923683.03350176 / 1000, 300074.043717263 / 1000, 5002833.32624071 / 1000, 0,0,0)
+centerEarth = StateVector(0, 0, 0, 0, 0, 0)
+dopStateVec = StateVector(3923683.03350176 / 1000, 300074.043717263 / 1000, 5002833.32624071 / 1000, 0, 0, 0)
 satellite = twoline2rv(line1, line2, wgs72)
+
+c = 299792 # km/s , value from google
+
 
 def getStateVectors(starttime, endtime, delta_seconds):
     state_vecs = []
-    currenttime = starttime
+    current_time = starttime
 
-    while (currenttime < endtime):
-        pos, vel = satellite.propagate(currenttime.year, currenttime.month, currenttime.day, currenttime.hour,
-                                            currenttime.minute, currenttime.second)
+    while current_time < endtime:
+        pos, vel = satellite.propagate(current_time.year, current_time.month, current_time.day, current_time.hour,
+                                       current_time.minute, current_time.second)
         state_vec = StateVector(pos[0], pos[1], pos[2], vel[0], vel[1], vel[2])
-        state_vec.rotate_time(currenttime)
+        state_vec.rotate_time(current_time)
         state_vecs.append(state_vec)
 
-        currenttime = currenttime + datetime.timedelta(0, delta_seconds)
+        current_time = current_time + datetime.timedelta(0, delta_seconds)
     return state_vecs
+
 
 def getDoptrackVec(starttime, endtime, delta_seconds):
     doptrack_vectors = []
     current_time = starttime
 
-    while (current_time < endtime):
-        doptrack_vec = StateVector(doptrack.x/1000, doptrack.y/1000, doptrack.z/1000, 0, 0, 0)
+    while current_time < endtime:
+        doptrack_vec = StateVector(doptrack.x / 1000, doptrack.y / 1000, doptrack.z / 1000, 0, 0, 0)
         doptrack_vec.rotate_time(current_time)
         doptrack_vectors.append(doptrack_vec)
 
         current_time = current_time + datetime.timedelta(0, delta_seconds)
     return doptrack_vectors
 
+
 def getRotatedDopVecs(start_time, end_time, delta_seconds):
     doptrack_vectors = []
     current_time = start_time
 
-    while (current_time < end_time):
+    while current_time < end_time:
         dop_vec = copy.copy(dopStateVec)
         dop_vec.rotate_time(current_time)
         doptrack_vectors.append(dop_vec)
@@ -67,108 +74,79 @@ def getRotatedDopVecs(start_time, end_time, delta_seconds):
         current_time = current_time + datetime.timedelta(0, delta_seconds)
     return doptrack_vectors
 
+
+def getEarthPoints():
+    ellipsoid = []
+    for lat in range(18):
+        for lon in range(36):
+            point = LatLon((lat * 10) - 90, (lon * 10) - 180).toCartesian()
+            point = StateVector(point.x / 1000, point.y / 1000, point.z / 1000, 0, 0, 0)
+            ellipsoid.append(point)
+    return ellipsoid
+
+
+def getPrimeMeridian():
+    prime_meridian_vernal_equinox = []
+    prime_meridian_at_start = []
+    for lat in range(18):
+        point_cartesian = LatLon((lat * 10) - 90, 0).toCartesian()
+        point = StateVector(point_cartesian.x / 1000, point_cartesian.y / 1000, point_cartesian.z / 1000, 0, 0, 0)
+        prime_meridian_vernal_equinox.append(point)
+        point_ = StateVector(point_cartesian.x / 1000, point_cartesian.y / 1000, point_cartesian.z / 1000, 0, 0, 0)
+        point_.rotate_time(startRecord)
+        prime_meridian_at_start.append(point_)
+    return prime_meridian_vernal_equinox, prime_meridian_at_start
+
+
+def getUnitVectors(sat_vecs, dop_vecs):
+    unit_vectors = []
+    for sat, dop in zip(sat_vecs, dop_vecs):
+        sat_to_dop = dop.minus(sat)
+        length = sat_to_dop.getRange()
+        unit_s_t_d = sat_to_dop.divide(length)
+        unit_vectors.append(unit_s_t_d)
+    return unit_vectors
+
+
+def getRangeRates(state_vectors, unit_vectors):
+    range_rates = []
+
+    for cur_unit, cur_vel in zip(unit_vectors, state_vectors):
+        range_rate = cur_unit.times(cur_unit.dotPosVel(cur_vel))
+        range_rates.append(range_rate)
+
+    return range_rates
+
+def getFrequencies(range_rates, f0):
+    frequencies = []
+
+    for range_rate in range_rates:
+        frequency = (1-range_rate / c) * f0
+        frequencies.append(frequency)
+    return frequencies
+
 state_vectors = getStateVectors(startRecord, endRecord, 10)
-doptrack_vectors = getRotatedDopVecs(startRecord, endRecord, 10) #getDoptrackVec(startRecord, endRecord, 10)
+doptrack_vectors = getRotatedDopVecs(startRecord, endRecord, 10)  # getDoptrackVec(startRecord, endRecord, 10)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-xs_sat = list(map(lambda x: x.posX(), state_vectors))
-ys_sat = list(map(lambda x: x.posY(), state_vectors))
-zs_sat = list(map(lambda x: x.posZ(), state_vectors))
-# ax.scatter(xs_sat, ys_sat, zs_sat)
+PlotHelper.scatterStatevectors(ax, state_vectors, color='b', marker='D')
+PlotHelper.scatterStatevectors(ax, doptrack_vectors)
 
-xs_doptrack = list(map(lambda x: x.posX(), doptrack_vectors))
-ys_doptrack = list(map(lambda x: x.posY(), doptrack_vectors))
-zs_doptrack = list(map(lambda x: x.posZ(), doptrack_vectors))
-ax.scatter(xs_doptrack, ys_doptrack, zs_doptrack)
+ellipsoid = getEarthPoints()
+PlotHelper.scatterStatevectors(ax, ellipsoid, color='g', marker='x')
 
+prime_meridian_vernal_equinox, prime_meridian_at_start = getPrimeMeridian()
 
-ellipsoid = []
+PlotHelper.scatterStatevectors(ax, prime_meridian_vernal_equinox, color='g')
+PlotHelper.scatterStatevectors(ax, prime_meridian_at_start, color='y')
 
-for lat in range(18):
-    for lon in range (36):
-        point = LatLon((lat*10) - 90, (lon*10) - 180)
-        ellipsoid.append(point.toCartesian())
+PlotHelper.plotAxesMarkers(ax)
 
-xs_earth = list(map(lambda x: x.x/1000, ellipsoid))
-ys_earth = list(map(lambda x: x.y/1000, ellipsoid))
-zs_earth = list(map(lambda x: x.z/1000, ellipsoid))
+unit_vectors = getUnitVectors(state_vectors, doptrack_vectors)
+range_rates = getRangeRates(state_vectors, unit_vectors)
 
-# Plot the earth
-# ax.scatter(xs_earth, ys_earth, zs_earth, color='r')
-
-prime_meridian_vernal_equinox = []
-prime_meridian_at_start = []
-
-for lat in range (18):
-    point_cartesian = LatLon((lat*10) - 90, 0).toCartesian()
-    point = StateVector(point_cartesian.x/1000, point_cartesian.y/1000, point_cartesian.z/1000,0,0,0)
-    prime_meridian_vernal_equinox.append(point)
-    point_ = StateVector(point_cartesian.x/1000, point_cartesian.y/1000, point_cartesian.z/1000,0,0,0)
-    point_.rotate_time(startRecord)
-    prime_meridian_at_start.append(point_)
-
-xs_prime = list(map(lambda x: x.posX(), prime_meridian_vernal_equinox))
-ys_prime = list(map(lambda x: x.posY(), prime_meridian_vernal_equinox))
-zs_prime = list(map(lambda x: x.posZ(), prime_meridian_vernal_equinox))
-ax.scatter(xs_prime, ys_prime, zs_prime, color='g')
-
-xs_prime_at_start = list(map(lambda x: x.posX(), prime_meridian_at_start))
-ys_prime_at_start = list(map(lambda x: x.posY(), prime_meridian_at_start))
-zs_prime_at_start = list(map(lambda x: x.posZ(), prime_meridian_at_start))
-ax.scatter(xs_prime_at_start, ys_prime_at_start, zs_prime_at_start, color='y')
-
-
-Axes3D.plot(ax, xs=[0,6000], ys=[0,0], zs=[0,0], c='r')
-Axes3D.plot(ax, xs=[0,0], ys=[0,6000], zs=[0,0], c='g')
-Axes3D.plot(ax, xs=[0,0], ys=[0,0], zs=[0,6000], c='b')
-
-unit_vectors = []
-
-for i in range(len(state_vectors)):
-    # vec_to_dop = doptrack_vectors[i].minus(state_vectors[i])
-    vec_to_dop = state_vectors[i].minus(doptrack_vectors[i])
-    length = np.sqrt(np.square(vec_to_dop.posX()) + np.square(vec_to_dop.posY()) + np.square(vec_to_dop.posZ()))
-    unit_v_t_d = StateVector(vec_to_dop.posX()/length, vec_to_dop.posY()/length, vec_to_dop.posZ()/length, 0, 0, 0)
-    unit_vectors.append(unit_v_t_d)
-
-range_rates = []
-
-for i in range(len(unit_vectors)):
-    cur_unit = unit_vectors[i]
-    cur_vel = state_vectors[i]
-    # range_rate = StateVector(cur_unit.posX()*cur_vel.velX(), cur_unit.posY()*cur_vel.velY(), cur_unit.posZ()*cur_vel.velZ(),0,0,0)
-    range_rate = cur_unit.times(cur_unit.dotPosVel(cur_vel))
-    range_rate.printLength()
-    range_rates.append(range_rate)
-
-for i in range(len(state_vectors)):
-    # Sat to doptrack
-    # Axes3D.plot(ax, xs=[state_vectors[i].posX(), doptrack_vectors[i].posX()],
-    #             ys=[state_vectors[i].posY(), doptrack_vectors[i].posY()],
-    #             zs=[state_vectors[i].posZ(), doptrack_vectors[i].posZ()], c='c')
-
-    #Sat + unit
-    # Axes3D.plot(ax, xs=[state_vectors[i].posX(), state_vectors[i].posX() + unit_vectors[i].posX()],
-    #             ys=[state_vectors[i].posY(), state_vectors[i].posY() + unit_vectors[i].posY()],
-    #             zs=[state_vectors[i].posZ(), state_vectors[i].posZ() + unit_vectors[i].posZ()])
-
-    #Sat + rangerate
-    Axes3D.plot(ax, xs=[state_vectors[i].posX(), state_vectors[i].posX() + range_rates[i].posX()],
-                ys=[state_vectors[i].posY(), state_vectors[i].posY() + range_rates[i].posY()],
-                zs=[state_vectors[i].posZ(), state_vectors[i].posZ() + range_rates[i].posZ()])
-
-
-    # Sat to center of earth
-    # Axes3D.plot(ax, xs=[state_vectors[i].posX(), 0],
-    #             ys=[state_vectors[i].posY(), 0],
-    #             zs=[state_vectors[i].posZ(), 0], c='m')
-
-    # Doptrack to center of earth
-    Axes3D.plot(ax, xs=[0, doptrack_vectors[i].posX()],
-                ys=[0, doptrack_vectors[i].posY()],
-                zs=[0, doptrack_vectors[i].posZ()])
+PlotHelper.plotLines(ax, state_vectors, doptrack_vectors)
 
 plt.show()
-
